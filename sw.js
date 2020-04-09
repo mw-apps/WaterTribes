830c29dfe22b86e02209a03298eaca1d0ef6dafe
mw-apps <63309946+mw-apps@users.noreply.github.com>
2020-04-09 17:14:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ﻿//workbox.precaching.precacheAndRoute(__precacheManifest)
 
-var cacheName = 'watertribes-sw-v2';
+var cacheName = 'watertribes-sw-v3';
 var filesToCache = [
     './index.html',
     './favicon.png',
